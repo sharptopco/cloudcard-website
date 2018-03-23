b03e83a9f0dc4585d0cbcb205dcc15386d92df25
Tony Erskine <tony@sharptop.co>
2018-03-23 12:32:56 -0400

    refactoring

--- passport-photos-pictures/home/home.js
+++ passport-photos-pictures/home/home.js
@@ -1,23 +1,9 @@
-var fileInput = null;
 var URL = 'https://' + API_URL + '/api/passport-photos';
 var data = {
     'email': null,
     'encodedImage': null
 }
 var defaultEmailAddress = 'tony@sharptop.io';
-
-function post(data) {
-    $.ajax({
-        type: 'POST',
-        url: URL,
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify(data),
-        success: function (response) {
-            console.log('response', response);
-        }
-    });
-}
 
 function showStepOne() {
     transitionFromTo('.step-two', '.step-one')
@@ -29,6 +15,11 @@ function showStepTwo() {
 
 function showStepThree() {
     transitionFromTo('.step-two', '.step-three')
+}
+
+function showSelectedImage(encodedImage) {
+    $('#selected-image').attr('src', encodedImage).width('75%');
+    transitionFromTo('#user-icon', '#selected-image');
 }
 
 function transitionFromTo(from, to) {
@@ -47,35 +38,52 @@ function pad(num) {
     return (num+"").padStart(2, '0')
 }
 
-function generateRandomEmailAddress() {
-    var username = defaultEmailAddress.split("@")[0];
-    var domain = defaultEmailAddress.split("@")[1];
+function getDateString() {
     var currentdate = new Date();
-    var datetime = pad(currentdate.getMonth()+1)  + "."
+    var datetime = pad(currentdate.getMonth() + 1) + "."
         + pad(currentdate.getDate()) + "_"
         + pad(currentdate.getHours())
         + pad(currentdate.getMinutes());
-    return username + "+" + datetime + "_" + makeid(4) + "@" + domain;
+    return datetime;
 }
 
-function onFileChange(element) {
-    fileInput = element;
+function generateRandomEmailAddress() {
+    var username = defaultEmailAddress.split("@")[0];
+    var domain = defaultEmailAddress.split("@")[1];
+    return username + "+" + getDateString() + "_" + makeid(4) + "@" + domain;
+}
+
+function onFileChange(fileInput) {
     if (fileInput.files && fileInput.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('#selected-image').attr('src', e.target.result).width('75%');
-            transitionFromTo('#user-icon', '#selected-image');
-            data.encodedImage = e.target.result;
-            var email = generateRandomEmailAddress();
-            console.log('email address: ', email);
-            data.email = email;
-            post(data);
+            var encodedImage = e.target.result;
+            postImage(encodedImage);
+            showSelectedImage(encodedImage);
         };
         reader.readAsDataURL(fileInput.files[0]);
-        setTimeout(function () {
-            // showStepTwo();
-        }, 1000);
     }
+}
+
+function postImage(encodedImage) {
+    data.encodedImage = encodedImage;
+    var email = generateRandomEmailAddress();
+    console.log('email address: ', email);
+    data.email = email;
+    post(data);
+}
+
+function post(data) {
+    $.ajax({
+        type: 'POST',
+        url: URL,
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function (response) {
+            console.log('response', response);
+        }
+    });
 }
 
 function submit() {
