34ba012f79956b1f978bb9f2e3b0b595b504e5c5
Brandon McCray <bradmccray89@gmail.com>
2018-04-06 12:01:35 -0400

    fixed picture alignment

--- passport-photos-pictures/download-photo/download-photo.js
+++ passport-photos-pictures/download-photo/download-photo.js
@@ -2,8 +2,6 @@ function initDownloadPhoto() {
     var photoKey = getPhotoKey()
     var photoURL = "//" + API_URL + "/api/passport-photos/" + photoKey + "/bytes";
 
-    // getWidthandHeight(photoURL);
-
     $('#download-photo-image').attr("src", photoURL);
     $('.download-link').attr("href", photoURL);
     $('.sqs-block-button-element').attr("href", photoURL);
@@ -18,22 +16,14 @@ function loadImagetoCanvas(){
     var photoKey = getPhotoKey()
     var photoURL = "//" + API_URL + "/api/passport-photos/" + photoKey + "/bytes";
     var img = new Image();
-    img.src = photoURL;
     img.crossOrigin = "Anonymous";
+    img.src = photoURL;
     img.onload = function(){
         var canvas = document.getElementById('myCanvas');
         var ctx = canvas.getContext('2d');
         ctx.drawImage(img, 200, 300, 600, 600);
         ctx.drawImage(img, 1000, 300, 600, 600);
         var fourBySixImg = canvas.toDataURL("image/png");
-        document.write('<img src="'+fourBySixImg+'"/>');
+        $('#four-by-six-image').attr("src", fourBySixImg);
     }
-};
-// function getWidthandHeight(url){
-//     $("<img/>",{
-//         load : function(){
-//             console.log("Width= " + this.width + " Height= "+ this.height);
-//         },
-//         src  : url
-//     });
-// }
+}
