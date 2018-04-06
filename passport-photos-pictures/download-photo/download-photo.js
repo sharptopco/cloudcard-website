2b365b0eeb3825a0ce0fee175eb1c1dc13f671ae
Brandon McCray <bradmccray89@gmail.com>
2018-04-06 11:09:22 -0400

    passport canvas photo

--- passport-photos-pictures/download-photo/download-photo.js
+++ passport-photos-pictures/download-photo/download-photo.js
@@ -1,6 +1,9 @@
 function initDownloadPhoto() {
     var photoKey = getPhotoKey()
     var photoURL = "//" + API_URL + "/api/passport-photos/" + photoKey + "/bytes";
+
+    // getWidthandHeight(photoURL);
+
     $('#download-photo-image').attr("src", photoURL);
     $('.download-link').attr("href", photoURL);
     $('.sqs-block-button-element').attr("href", photoURL);
@@ -11,3 +14,25 @@ function initDownloadPhoto() {
         $('.sqs-block-button-element').text("Download Photo");
     }, 1000);
 }
+function loadImagetoCanvas(){
+    var photoKey = getPhotoKey()
+    var photoURL = "//" + API_URL + "/api/passport-photos/" + photoKey + "/bytes";
+    var img = new Image();
+    img.src = photoURL;
+    img.onload = function(){
+        var canvas = document.getElementById('myCanvas');
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 200, 300, 600, 600);
+        ctx.drawImage(img, 1000, 300, 600, 600);
+        var fourBySixImg = canvas.toDataURL("image/png");
+        document.write('<img src="'+fourBySixImg+'"/>');
+    }
+};
+// function getWidthandHeight(url){
+//     $("<img/>",{
+//         load : function(){
+//             console.log("Width= " + this.width + " Height= "+ this.height);
+//         },
+//         src  : url
+//     });
+// }
