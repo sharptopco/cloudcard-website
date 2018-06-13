ef12339b2916b9f421c78e1394106fa6bab9f9b4
Brandon McCray <bradmccray89@gmail.com>
2018-06-13 11:26:20 -0400

    Reverted image to normal jpg

--- passport-photos-pictures/download-photo/download-photo.js
+++ passport-photos-pictures/download-photo/download-photo.js
@@ -1,10 +1,6 @@
 function initDownloadPhoto() {
     var photoURL = getPhotoURL();
 
-    // createImage(photoURL)
-    // $('#download-photo-image').attr("src", photoURL);
-    // $('.download-link').attr("href", photoURL);
-    //
     setTimeout(function () {
         var downloadPhotoContent = $('#download-photo-content');
         transitionFromTo($('#loading'), downloadPhotoContent);
@@ -36,46 +32,18 @@ function create4x6Image(photoURL){
     img.onload = function(){
         var fourBySixCanvas = document.getElementById('fourBySixCanvas');
         var ctx2 = fourBySixCanvas.getContext('2d');
-
-        var width1 = ctx2.canvas.width;
-        var height1 = ctx2.canvas.height;
-        var data1 = ctx2.getImageData(0, 0, width1, height1);
-        var compositeOperation1 = ctx2.globalCompositeOperation;
-        ctx2.globalCompositeOperation = "destination-over";
-        ctx2.fillStyle = "#FFFFFF";
-        ctx2.fillRect(0,0,width1,height1);
-
         ctx2.drawImage(img, 200, 300, 600, 600);
         ctx2.drawImage(img, 1000, 300, 600, 600);
         ctx2.strokeRect(200, 300, 600, 600);
         ctx2.strokeRect(1000, 300, 600, 600);
         var fourBySixImg = fourBySixCanvas.toDataURL("image/jpeg");
-
-        ctx2.clearRect (0,0,width1,height1);
-        ctx2.putImageData(data1, 0,0);
-        ctx2.globalCompositeOperation = compositeOperation1;
-
         $('#four-by-six-image').attr("src", fourBySixImg);
 
         var twoByTwoCanvas = document.getElementById('twoByTwoCanvas');
         var ctx1 = twoByTwoCanvas.getContext('2d');
-
-        var width2 = ctx1.canvas.width;
-        var height2 = ctx1.canvas.height;
-        var data2 = ctx1.getImageData(0, 0, width2, height2);
-        var compositeOperation2 = ctx1.globalCompositeOperation;
-        ctx1.globalCompositeOperation = "destination-over";
-        ctx1.fillStyle = "#FFFFFF";
-        ctx1.fillRect(0,0,width,height);
-
         ctx1.drawImage(img, 600, 300, 600, 600);
         ctx1.strokeRect(600, 300, 600, 600);
         var twoByTwoImg = twoByTwoCanvas.toDataURL("image/jpeg");
-
-        ctx1.clearRect (0,0,width2,height2);
-        ctx1.putImageData(data2, 0,0);
-        ctx1.globalCompositeOperation = compositeOperation2;
-
         $('#two-by-two-image').attr("src", twoByTwoImg);
     }
 }
