588f42f714a9f6d471e3a51301c4409139b69ee2
Brandon McCray <bradmccray89@gmail.com>
2018-06-13 11:15:23 -0400

    Changed photo background color to white

--- passport-photos-pictures/download-photo/download-photo.js
+++ passport-photos-pictures/download-photo/download-photo.js
@@ -36,35 +36,49 @@ function create4x6Image(photoURL){
     img.onload = function(){
         var fourBySixCanvas = document.getElementById('fourBySixCanvas');
         var ctx2 = fourBySixCanvas.getContext('2d');
+
+        var width = ctx2.canvas.width;
+        var height = ctx2.canvas.height;
+        var data = ctx2.getImageData(0, 0, width, height);
+        var compositeOperation = ctx2.globalCompositeOperation;
+        ctx2.globalCompositeOperation = "destination-over";
+        ctx2.fillStyle = "#FFFFFF";
+        ctx2.fillRect(0,0,w,h);
+
         ctx2.drawImage(img, 200, 300, 600, 600);
         ctx2.drawImage(img, 1000, 300, 600, 600);
         ctx2.strokeRect(200, 300, 600, 600);
         ctx2.strokeRect(1000, 300, 600, 600);
         var fourBySixImg = fourBySixCanvas.toDataURL("image/jpeg");
+
+        ctx2.clearRect (0,0,w,h);
+        ctx2.putImageData(data, 0,0);
+        ctx2.globalCompositeOperation = compositeOperation;
+
         $('#four-by-six-image').attr("src", fourBySixImg);
 
         var twoByTwoCanvas = document.getElementById('twoByTwoCanvas');
         var ctx1 = twoByTwoCanvas.getContext('2d');
+
+        var width = ctx1.canvas.width;
+        var height = ctx1.canvas.height;
+        var data = ctx1.getImageData(0, 0, width, height);
+        var compositeOperation = ctx1.globalCompositeOperation;
+        ctx1.globalCompositeOperation = "destination-over";
+        ctx1.fillStyle = "#FFFFFF";
+        ctx1.fillRect(0,0,w,h);
+
         ctx1.drawImage(img, 600, 300, 600, 600);
         ctx1.strokeRect(600, 300, 600, 600);
         var twoByTwoImg = twoByTwoCanvas.toDataURL("image/jpeg");
+
+        ctx1.clearRect (0,0,w,h);
+        ctx1.putImageData(data, 0,0);
+        ctx1.globalCompositeOperation = compositeOperation;
+
         $('#two-by-two-image').attr("src", twoByTwoImg);
     }
 }
-
-// function createImage(photoURL){
-//     var img = new Image();
-//     img.crossOrigin = "Anonymous";
-//     img.src = photoURL;
-//     img.onload = function(){
-//         var canvas = document.getElementById('myCanvas');
-//         var ctx = canvas.getContext('2d');
-//         ctx.drawImage(img, 600, 300, 600, 600);
-//         ctx.strokeRect(600, 300, 600, 600);
-//         var twoByTwoImg = canvas.toDataURL("image/png");
-//         $('#two-by-two-image').attr("src", twoByTwoImg);
-//     }
-// }
 
 function setupSquarespaceButton(photoURL) {
     var squareSpaceButton = $('.sqs-block-button-element');
