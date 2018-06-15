541a254040677eb89320daa6fbfb610400299af6
Brandon McCray <bradmccray89@gmail.com>
2018-06-14 20:07:11 -0400

    changed photoURL to photoKey in paypal transaction

--- passport-photos-pictures/constants.js
+++ passport-photos-pictures/constants.js
@@ -1,4 +1,3 @@
 var API_URL = "api.onlinephotosubmission.com"
 var PROTOCOL = "https://";
 var URL = PROTOCOL + API_URL + '/api/passport-photos';
-var PHOTO_URL = PROTOCOL + "www.passportphotos.pictures/download-photo";
